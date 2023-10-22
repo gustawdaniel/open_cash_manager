95ccc81af95ebc5c2bc23633526f973ad19fd628
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-10-22 09:52:52 +0400

    added console log

--- apps/ui/app.vue
+++ apps/ui/app.vue
@@ -18,6 +18,7 @@ function notOurDomain(origin: string): boolean {
 window.addEventListener(
   'message',
   (event) => {
+    console.log(event);
     if (notOurDomain(event.origin)) return;
 
     if ('debug' in event.data && typeof event.data.debug === 'boolean') {
