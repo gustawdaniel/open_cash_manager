cb23af8f98e1ef258c109ecb955196a99bda745c
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-12-16 20:32:00 +0400

    added copy of transaction format date support filter of closed/hidden accounts

--- apps/ui/pages/transaction/[id].vue
+++ apps/ui/pages/transaction/[id].vue
@@ -1,19 +1,53 @@
 <script lang="ts" setup>
 import { useRoute, useRouter } from '#imports';
 import SingleTransactionEdit from '~/components/transactions/SingleTransactionEdit.vue';
-import { useTransactionStore } from '~/store/transaction';
+import { type FullTransaction, useTransactionStore } from '~/store/transaction';
+import { useAccountStore } from '~/store/account';
 
 const route = useRoute();
 const router = useRouter();
+
+console.log(route.query.account);
 
 const NEW_TRANSACTION_ID = 'new';
 
 const transactionId: string = String(route.params.id);
 
 const transactionStore = useTransactionStore();
+const accountStore = useAccountStore();
+
+function newTransaction(): FullTransaction {
+  const transaction = transactionStore.getNew();
+
+  if (route.query.account) {
+    const account = accountStore.getById(String(route.query.account));
+    if (account) {
+      transaction.account = account.name;
+      transaction.accountId = account.id;
+    }
+  }
+
+  if (route.query.copy) {
+    const original = transactionStore.getById(String(route.query.copy));
+    if (original) {
+      transaction.account = original.account;
+      transaction.accountId = original.accountId;
+      transaction.date = original.date;
+      transaction.amount = original.amount;
+      transaction.category = original.category;
+      transaction.clearedStatus = original.clearedStatus;
+      transaction.memo = original.memo;
+      transaction.payee = original.payee;
+      transaction.transferHash = original.transferHash;
+    }
+  }
+
+  return transaction;
+}
+
 const transaction =
   transactionId === NEW_TRANSACTION_ID
-    ? transactionStore.getNew()
+    ? newTransaction()
     : transactionStore.getById(transactionId);
 const reverseTransaction = transactionStore.getReverseByIdAndHash(
   transactionId,
