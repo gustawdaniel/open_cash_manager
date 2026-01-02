cc65fcdbbf0ba61cfd4842d5879d3038d2acf538
gustawdaniel <gustaw.daniel@gmail.com>
2026-01-02 17:10:38 +0100

    feat: Add `zod` and `zod-to-json-schema` to docs dependencies and apply non-null assertions in UI seed data.

--- apps/ui/utils/seed.ts
+++ apps/ui/utils/seed.ts
@@ -38,8 +38,8 @@ export function seedDemoData() {
   const transactionStore = useTransactionStore();
 
   transactionStore.create({
-    accountId: accounts[0].id,
-    account: accounts[0].name,
+    accountId: accounts[0]!.id,
+    account: accounts[0]!.name,
     amount: 3000,
     date: dayjs().add(-11, 'days').format('YYYY-MM-DD'),
     payee: 'Opening value',
@@ -47,8 +47,8 @@ export function seedDemoData() {
   });
 
   transactionStore.create({
-    accountId: accounts[1].id,
-    account: accounts[1].name,
+    accountId: accounts[1]!.id,
+    account: accounts[1]!.name,
     amount: 0.33429901,
     date: dayjs().add(-11, 'days').format('YYYY-MM-DD'),
     payee: 'Opening value',
@@ -56,8 +56,8 @@ export function seedDemoData() {
   });
 
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 5000,
     date: dayjs().add(-11, 'days').format('YYYY-MM-DD'),
     payee: 'Opening value',
@@ -65,8 +65,8 @@ export function seedDemoData() {
   });
 
   transactionStore.create({
-    accountId: accounts[3].id,
-    account: accounts[3].name,
+    accountId: accounts[3]!.id,
+    account: accounts[3]!.name,
     amount: 2500,
     date: dayjs().add(-11, 'days').format('YYYY-MM-DD'),
     payee: 'Opening value',
@@ -75,8 +75,8 @@ export function seedDemoData() {
 
   // this is an example of expense
   transactionStore.create({
-    accountId: accounts[1].id,
-    account: accounts[1].name,
+    accountId: accounts[1]!.id,
+    account: accounts[1]!.name,
     amount: -0.00001,
     date: dayjs().add(-6, 'days').format('YYYY-MM-DD'),
     payee: 'My first pizza',
@@ -85,27 +85,27 @@ export function seedDemoData() {
 
   // these two below are transfer
   transactionStore.create({
-    accountId: accounts[3].id,
-    account: accounts[3].name,
+    accountId: accounts[3]!.id,
+    account: accounts[3]!.name,
     amount: -100,
     date: dayjs().add(-6, 'days').format('YYYY-MM-DD'),
     payee: 'From bank to cash',
-    category: `[${accounts[2].name}]`,
+    category: `[${accounts[2]!.name}]`,
   });
 
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 100,
     date: dayjs().add(-6, 'days').format('YYYY-MM-DD'),
     payee: 'From bank to cash',
-    category: `[${accounts[3].name}]`,
+    category: `[${accounts[3]!.name}]`,
   });
 
   // there is an example of income
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 2000,
     date: dayjs().add(-5, 'days').format('YYYY-MM-DD'),
     payee: 'Salary from my company',
@@ -117,27 +117,27 @@ export function seedDemoData() {
 
   // Create a transfer transaction from bank to cash
   transactionStore.create({
-    accountId: accounts[3].id,
-    account: accounts[3].name,
+    accountId: accounts[3]!.id,
+    account: accounts[3]!.name,
     amount: -500,
     date: dayjs().add(-4, 'days').format('YYYY-MM-DD'),
     payee: 'Withdraw cash for UFO fuel',
-    category: `[${accounts[2].name}]`,
+    category: `[${accounts[2]!.name}]`,
   });
 
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 500,
     date: dayjs().add(-4, 'days').format('YYYY-MM-DD'),
     payee: 'Withdraw cash for UFO fuel',
-    category: `[${accounts[3].name}]`,
+    category: `[${accounts[3]!.name}]`,
   });
 
   // Create an income transaction
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 3000,
     date: dayjs().add(-3, 'days').format('YYYY-MM-DD'),
     payee: 'Space Oddity Music Streaming',
@@ -146,8 +146,8 @@ export function seedDemoData() {
 
   // Create another expense transaction
   transactionStore.create({
-    accountId: accounts[1].id,
-    account: accounts[1].name,
+    accountId: accounts[1]!.id,
+    account: accounts[1]!.name,
     amount: -0.005,
     date: dayjs().add(-2, 'days').format('YYYY-MM-DD'),
     payee: 'Intergalactic Coffee Shop',
@@ -156,27 +156,27 @@ export function seedDemoData() {
 
   // Create a transfer transaction from cash to bank
   transactionStore.create({
-    accountId: accounts[0].id,
-    account: accounts[0].name,
+    accountId: accounts[0]!.id,
+    account: accounts[0]!.name,
     amount: -1000,
     date: dayjs().add(-1, 'days').format('YYYY-MM-DD'),
     payee: 'Deposit space credits',
-    category: `[${accounts[2].name}]`,
+    category: `[${accounts[2]!.name}]`,
   });
 
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 955,
     date: dayjs().add(-1, 'days').format('YYYY-MM-DD'),
     payee: 'Deposit space credits',
-    category: `[${accounts[0].name}]`,
+    category: `[${accounts[0]!.name}]`,
   });
 
   // Create an expense transaction for a Martian souvenir
   transactionStore.create({
-    accountId: accounts[0].id,
-    account: accounts[0].name,
+    accountId: accounts[0]!.id,
+    account: accounts[0]!.name,
     amount: -42.99,
     date: dayjs().add(-10, 'days').format('YYYY-MM-DD'),
     payee: 'The Martian Gift Shop',
@@ -185,27 +185,27 @@ export function seedDemoData() {
 
   // Create a transfer transaction from cash to Bitcoin wallet
   transactionStore.create({
-    accountId: accounts[0].id,
-    account: accounts[0].name,
+    accountId: accounts[0]!.id,
+    account: accounts[0]!.name,
     amount: -500,
     date: dayjs().add(-9, 'days').format('YYYY-MM-DD'),
     payee: 'Add BTC to wallet',
-    category: `[${accounts[1].name}]`,
+    category: `[${accounts[1]!.name}]`,
   });
 
   transactionStore.create({
-    accountId: accounts[1].id,
-    account: accounts[1].name,
+    accountId: accounts[1]!.id,
+    account: accounts[1]!.name,
     amount: 0.0155,
     date: dayjs().add(-9, 'days').format('YYYY-MM-DD'),
     payee: 'Add BTC to wallet',
-    category: `[${accounts[0].name}]`,
+    category: `[${accounts[0]!.name}]`,
   });
 
   // Create an income transaction from a freelance gig
   transactionStore.create({
-    accountId: accounts[2].id,
-    account: accounts[2].name,
+    accountId: accounts[2]!.id,
+    account: accounts[2]!.name,
     amount: 1500,
     date: dayjs().add(-8, 'days').format('YYYY-MM-DD'),
     payee: 'Galactic Freelance Co.',
@@ -214,8 +214,8 @@ export function seedDemoData() {
 
   // Create an expense transaction for interstellar fuel
   transactionStore.create({
-    accountId: accounts[3].id,
-    account: accounts[3].name,
+    accountId: accounts[3]!.id,
+    account: accounts[3]!.name,
     amount: -75.5,
     date: dayjs().add(-7, 'days').format('YYYY-MM-DD'),
     payee: 'Fusion Fuel Station',
@@ -224,8 +224,8 @@ export function seedDemoData() {
 
   // Create a transfer transaction from cash to bank for space rent
   transactionStore.create({
-    accountId: accounts[0].id,
-    account: accounts[0].name,
+    accountId: accounts[0]!.id,
+    account: accounts[0]!.name,
     amount: -800,
     date: dayjs().add(-6, 'days').format('YYYY-MM-DD'),
     payee: 'Pay rent for Starship Apartment',
@@ -234,8 +234,8 @@ export function seedDemoData() {
 
   // Create an income transaction from a moonwalking performance
   transactionStore.create({
-    accountId: accounts[1].id,
-    account: accounts[1].name,
+    accountId: accounts[1]!.id,
+    account: accounts[1]!.name,
     amount: -0.0000025,
     date: dayjs().add(-5, 'days').format('YYYY-MM-DD'),
     payee: 'Moonwalk Extravaganza Show',
