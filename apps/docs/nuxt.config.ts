2c702b384d93a58ed18130d281d6a84b91366f90
Daniel Gustaw <gustaw.daniel@gmail.com>
2026-02-15 12:50:12 +0100

    fix

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -1,4 +1,15 @@
 export default defineNuxtConfig({
     extends: 'docus',
-    compatibilityDate: '2025-02-15'
+    compatibilityDate: '2025-02-15',
+    nitro: {
+        prerender: {
+            failOnError: false,
+            ignore: [
+                '/gustawdaniel/vault-track',
+                '/raw/introduction/_dir.md',
+                '/raw/files/_dir.md',
+                '/raw/developer-guide/_dir.md'
+            ]
+        }
+    }
 })
