11ed73e78c85641be26531ade0f7d35c0b7562a5
Daniel Gustaw <gustaw.daniel@gmail.com>
2026-02-15 16:02:04 +0100

    feat: Add transaction CSV import/export, bulk deletion, and assert backup/restore.

--- apps/ui/components/backup/getExportContent.ts
+++ apps/ui/components/backup/getExportContent.ts
@@ -3,11 +3,13 @@ import { type QifAccount, QifType, serializeQif } from 'qif-ts';
 import { useAccountStore } from '~/store/account';
 import { useTransactionStore } from '~/store/transaction';
 import { useCategoryStore } from '~/store/category';
+import { useAssertStore } from '~/store/assert';
 
 export function getExportContent(type: 'qif' | 'json') {
   const accountStore = useAccountStore();
   const transactionStore = useTransactionStore();
   const categoryStore = useCategoryStore();
+  const assertStore = useAssertStore();
 
   switch (type) {
     case 'qif':
@@ -21,6 +23,7 @@ export function getExportContent(type: 'qif' | 'json') {
         transactions: transactionStore.transactions,
         accounts: accountStore.accounts,
         categories: categoryStore.categories,
+        asserts: assertStore.asserts,
       });
   }
 }
