7fd9c84d97da69ff32ad521aafad49f7bee2e31d
Daniel Gustaw <gustaw.daniel@gmail.com>
2026-02-15 14:26:07 +0100

    provider none

--- apps/docs/nuxt.config.ts
+++ apps/docs/nuxt.config.ts
@@ -1,6 +1,9 @@
 export default defineNuxtConfig({
     extends: 'docus',
     compatibilityDate: '2025-02-15',
+    image: {
+        provider: 'none',
+    },
     nitro: {
         preset: 'static',
         prerender: {
